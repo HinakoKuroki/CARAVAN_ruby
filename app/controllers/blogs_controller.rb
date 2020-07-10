a7faac147da151664a4f326c1ce5c8aeac7e8388
Hinako Kuroki <pi.pi.hinako.29@gmail.com>
2020-07-10 20:20:55 +0900

    [Start]4-3

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -5,7 +5,11 @@ class BlogsController < ApplicationController
   def show
   end
 
-  def new
+  def new #投稿フォームを表示
+    @blog = Blog.new #blogインスタンスをBlogモデルから作成
+  end
+
+  def create #newアクションで表示されたフォームでの投稿内容を保存
   end
 
   def edit
